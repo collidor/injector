0d38ec1744694c7522e51abbdb61afdb76f69778
Alykam Burdzaki <alykam@gmail.com>
2025-02-25 08:06:14 +0100

    add unregister

--- src/injector.ts
+++ src/injector.ts
@@ -1,32 +1,36 @@
 export type Type<T> = new (...args: any[]) => T;
 
 export class Injector {
-    private instances = new Map<any, any>();
+  private instances = new Map<any, any>();
 
-    public register = <T = any, Q = any>(type: Q, instance: T): void => {
-        this.instances.set(type, instance);
-    };
+  public register = <T = any, Q = any>(type: Q, instance: T): void => {
+    this.instances.set(type, instance);
+  };
 
-    public inject = <
-        T,
-        Q = T extends Type<infer M> ? M
-            : T extends (...args: unknown[]) => infer R ? R
-            : any,
-    >(
-        type: T,
-    ): Q => {
-        if (!this.instances.has(type)) {
-            throw new Error(`No instance registered for class ${type}`);
-        }
-        return this.instances.get(type) as Q;
-    };
+  public inject = <
+    T,
+    Q = T extends Type<infer M> ? M
+      : T extends (...args: unknown[]) => infer R ? R
+      : any,
+  >(
+    type: T,
+  ): Q => {
+    if (!this.instances.has(type)) {
+      throw new Error(`No instance registered for class ${type}`);
+    }
+    return this.instances.get(type) as Q;
+  };
 
-    public get = <
-        T,
-        Q = T extends Type<infer M> ? M
-            : T extends (...args: unknown[]) => infer R ? R
-            : any,
-    >(
-        type: T,
-    ): Q => this.inject(type);
+  public unregister = <T = any>(type: T): void => {
+    this.instances.delete(type);
+  };
+
+  public get = <
+    T,
+    Q = T extends Type<infer M> ? M
+      : T extends (...args: unknown[]) => infer R ? R
+      : any,
+  >(
+    type: T,
+  ): Q => this.inject(type);
 }
--- src/main.test.ts
+++ src/main.test.ts
@@ -2,24 +2,37 @@ import { assertEquals, assertThrows } from "jsr:@std/assert";
 import { Injector } from "./main.ts";
 
 Deno.test("Injector - should register and get value", () => {
-    const injector = new Injector();
+  const injector = new Injector();
 
-    injector.register(Injector, injector);
-    assertEquals(injector.get(Injector), injector);
+  injector.register(Injector, injector);
+  assertEquals(injector.get(Injector), injector);
 
-    function a() {
-        return 1;
-    }
+  function a() {
+    return 1;
+  }
 
-    injector.register(a, a());
-    assertEquals(injector.get(a), 1);
+  injector.register(a, a());
+  assertEquals(injector.get(a), 1);
 });
 
 Deno.test("Injector - should throw if no instance is registered", () => {
-    const injector = new Injector();
+  const injector = new Injector();
 
-    assertThrows(
-        () => injector.get(Injector),
-        Error,
-    );
+  assertThrows(
+    () => injector.get(Injector),
+    Error,
+  );
+});
+
+Deno.test("Injector - should unregister instance", () => {
+  const injector = new Injector();
+
+  injector.register(Injector, injector);
+  assertEquals(injector.get(Injector), injector);
+
+  injector.unregister(Injector);
+  assertThrows(
+    () => injector.get(Injector),
+    Error,
+  );
 });
